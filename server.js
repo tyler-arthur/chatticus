453a668787617fa08b8d0aef466838dd9d146505
Swedish <tarthurf@yahoo.com>
2020-07-28 14:51:21 -0700

    changed proxies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io") (server);
@@ -11,6 +11,8 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+} else {
+  app.use(express.static("client/public"));
 }
 
 // Define API routes here
