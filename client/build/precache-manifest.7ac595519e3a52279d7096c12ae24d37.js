eec96386f7816a3ef6fba69eaddc2b5e15a8e232
Swedish <tarthurf@yahoo.com>
2020-08-15 13:14:13 -0700

    created production build for app

---
+++ client/build/precache-manifest.7ac595519e3a52279d7096c12ae24d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5820ad00c8bbd7f62bdd41e9455443c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d78be836be1f09917f",
+    "url": "/static/css/main.962a1751.chunk.css"
+  },
+  {
+    "revision": "aee3b1302dc63f189782",
+    "url": "/static/js/2.9a06df58.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.9a06df58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d78be836be1f09917f",
+    "url": "/static/js/main.66ee9e0a.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
